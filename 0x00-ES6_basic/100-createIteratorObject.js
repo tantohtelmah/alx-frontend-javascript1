31fc2ee36c37c916da83605826de493efde4ce37
tantohtelmah <tantohtelmah@gmail.com>
2024-06-24 21:38:19 -0400

    tasks

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function* createIteratorObject(report) {
+	for (const department in report.allEmployees) {
+	  const employees = report.allEmployees[department];
+	  for (const employee of employees) {
+		yield employee;
+	  }
+	}
+  }
+  
---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,5 @@
+export default function iterateThroughObject(reportWithIterator) {
+	const employeesArray = [...reportWithIterator];
+	return employeesArray.join(' | ');
+  }
+  
