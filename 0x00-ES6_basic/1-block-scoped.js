f2e8250633ec0b5fe8edee0ff527da2f8db3c2f2
tantohtelmah <tantohtelmah@gmail.com>
2024-06-25 22:08:42 -0400

    tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
 export default function taskBlock(trueOrFalse) {
-	let task = false; // Use 'let' instead of 'var'
-	let task2 = true; // Use 'let' instead of 'var'
-  
-	if (trueOrFalse) {
+  const task = false; // Use 'let' instead of 'var'
+  const task2 = true; // Use 'let' instead of 'var'
+
+  if (trueOrFalse) {
 	  // These variables won't overwrite the outer ones
-	  let task = true;
-	  let task2 = false;
-	}
-  
-	return [task, task2];
   }
-  
+
+  return [task, task2];
+}
